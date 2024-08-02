eb12b5da32f0a870e67e5740da4053424da14b6c
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-08-02 16:32:51 -0600

    Update BASE_URL for development

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import UserGrid from "./components/UserGrid"
 import { useState } from "react";
 
 
-export const BASE_URL = "http://127.0.0.1:5000/api";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
 
 function App() {
 
